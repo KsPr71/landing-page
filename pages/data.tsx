98ab0502e9b2eef540d51938f27eb98fff1a584a
Jorge Alejandro Casares Delgado <jorgealejandrocasaresdelgado@gmail.com>
2026-05-23 12:57:32 -0400

    mejorando

--- pages/data.tsx
+++ pages/data.tsx
@@ -14,13 +14,6 @@ const teamMembers = [
       image: "https://djxvosobflwtryenqoou.supabase.co/storage/v1/object/public/diseno/jeidy.jpg",
       bio: "Experta en diseño de imagenes."
     },
-    {
-      id: "3",
-      name: "Leiddy Cárdenas",
-      role: "Innovación y Desarrollo",
-      image: "https://lnixgnibuvvtbhmwurrx.supabase.co/storage/v1/object/sign/Aleatorio/leiddy.webp?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV9iNDNiMzc0MC0zMjQzLTQzZDEtOTM5Mi04MDQ5MWYyN2RiZWYiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJBbGVhdG9yaW8vbGVpZGR5LndlYnAiLCJpYXQiOjE3NzQ3MjM5OTYsImV4cCI6MzM1MTUyMzk5Nn0.JDbehE9i6kkXXwI1YegnjdPvYf2pxa_pVEO1m1nx6lA",
-      bio: "Programadora full stack con más de cinco años de experiencia."
-    },
 
     
     // ... more members
